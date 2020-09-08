4cc926ce9d1c61f631e5bd267d3de6028a54f8be
Emizzari <emilie@tonnessen.com>
2020-09-08 12:03:24 +0200

    Carousel

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import logoOne from './images/carousel-1.jpg';
+import logoTwo from './images/carousel-2.jpg';
+import logoThree from './images/carousel-3.jpg';
+
+console.log(logoOne);
+console.log(logoTwo);
+console.log(logoThree);
 
 function Home() {
     return (
-        <div className="test">
-            Home Component
-        </div>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={logoOne}
+                    alt="First slide"
+                />
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={logoTwo}
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={logoThree}
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+        </Carousel>
     )
 }
 
