7c863c265e7cfe4f9521262f126505a23038b97d
Emizzari <emilie@tonnessen.com>
2020-09-08 11:29:52 +0200

    Whitespace fix

--- src/App.js
+++ src/App.js
@@ -31,29 +31,9 @@ function App() {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        {/* <ul>
-                    <li>
-                        <NavLink exact to="/">
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about">About</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/dashboard">Dashboard</NavLink>
-                    </li>
-                </ul> */}
+      
 
-        <hr />
-
-        {/*
-        A <Switch> looks through all its children <Route>
-        elements and renders the first one whose path
-        matches the current URL. Use a <Switch> any time
-        you have multiple routes, but you want only one
-        of them to render at a time
-      */}
+      
         <Switch>
           <Route exact path="/">
             <Home />
@@ -64,7 +44,7 @@ function App() {
           <Route path="/contact">
             <Contact />
           </Route>
-        </Switch>
+        </Switch> 
       </div>
     </Router>
   );
