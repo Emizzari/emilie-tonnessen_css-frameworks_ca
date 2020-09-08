b128849b52443fa4ed75f6a90a47d509c647529b
Emizzari <emilie@tonnessen.com>
2020-09-08 12:04:12 +0200

    Renaming filepath
    Enabling sass to be used

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.scss';
+import "./sass/style.scss";
 import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 import Home from './components/home/Home';
 import News from './components/news/News';
