70ae5f1eba8607c471041636393768bf22b0b33e
Emizzari <emilie@tonnessen.com>
2020-09-08 13:26:39 +0200

    Accordion for the homepage
    Adding Accordion for the homepage

--- src/components/home/ContentAccordion.js
+++ src/components/home/ContentAccordion.js
@@ -7,51 +7,71 @@ import TabImage from './images/tab-image.png';
 function ContentAccordion() {
     return (
         <>
-        <Accordion defaultActiveKey="0">
-            <Card>
-                <Card.Header>
-                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                        First
-                    </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                    <Card.Img variant="top" src={TabImage} />
-                    <Card.Body>
-                        Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. 
-                    </Card.Body>
-                    <Card.Link href="#"><i class="fab fa-facebook-f"></i></Card.Link>
-                    <Card.Link href="#"><i class="fab fa-twitter"></i></Card.Link>
-                </Accordion.Collapse>
-            </Card>
-
-            <Card>
-                <Card.Header>
-                    <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                        Second
-                    </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="1">
-                    <Card.Img variant="top" src={TabImage} />
-                    <Card.Body>
-                        Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
-                    </Card.Body>
-                </Accordion.Collapse>
-            </Card>
-
-            <Card>
-                <Card.Header>
-                    <Accordion.Toggle as={Button} variant="link" eventKey="2">
-                        Third
-                    </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="2">
-                    <Card.Img variant="top" src={TabImage} />
-                    <Card.Body>
-                        Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
-                    </Card.Body>
-                </Accordion.Collapse>
-            </Card>
-        </Accordion>
+            <Accordion defaultActiveKey="0">
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                            First
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                        <>
+                        <Card.Body>
+                            <Card.Img variant="top" src={TabImage} />
+                            <Card.Text>
+                                Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. 
+                            </Card.Text>
+                            <Card.Link href="#">
+                                <i className="fab fa-facebook-f"></i>
+                                <i className="fab fa-twitter"></i>
+                            </Card.Link>
+                        </Card.Body>
+                        </>
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                            Second
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="1">
+                        <>
+                        <Card.Body>
+                            <Card.Img variant="top" src={TabImage} />
+                            <Card.Text>
+                                Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
+                            </Card.Text>
+                            <Card.Link href="#">
+                                <i className="fab fa-facebook-f"></i>
+                                <i className="fab fa-twitter"></i>
+                            </Card.Link>
+                        </Card.Body>
+                        </>
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                            Third
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="2">
+                        <>
+                        <Card.Body>
+                            <Card.Img variant="top" src={TabImage} />
+                            <Card.Text>
+                                Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. 
+                            </Card.Text>
+                            <Card.Link href="#">
+                                <i className="fab fa-facebook-f"></i>
+                                <i className="fab fa-twitter"></i>
+                            </Card.Link>
+                        </Card.Body>
+                        </>
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
         </>
     )
 }
