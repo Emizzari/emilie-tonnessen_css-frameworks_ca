89b85ba108e2fc490cdeff0b5a41d3353f60ea65
Emizzari <emilie@tonnessen.com>
2020-09-10 11:57:45 +0200

    Styling tabs

--- src/components/home/content/ContentTabs.js
+++ src/components/home/content/ContentTabs.js
@@ -11,43 +11,54 @@ function ContentTabs() {
         <Tabs defaultActiveKey="first">
             <Tab eventKey="first" title="First">
                 <Card>
-                    <Card.Body>
-                        <Card.Img variant="top" src={TabImage} />
-                        <Card.Text>
-                            Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
-                        </Card.Text>
-                        <Card.Link href="#">
-                            <i className="fab fa-facebook-f"></i>
-                            <i className="fab fa-twitter"></i>
-                        </Card.Link>
+                    <Card.Body className="tab">
+                        <Card.Img variant="top" src={TabImage} className="tab__img"/>
+                        <div className="tab__content">
+                            <Card.Text className="tab__text">
+                                Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
+                            </Card.Text>
+                            <Card.Link href="#" className="tab__icons">
+                                    <p>SHARE</p>
+                                    <i className="fab fa-facebook-f"></i>
+                                    <i className="fab fa-twitter"></i>
+                            </Card.Link>
+                        </div>
                     </Card.Body>
                 </Card>
             </Tab>
             <Tab eventKey="second" title="Second">
                 <Card>
-                    <Card.Body>
-                        <Card.Img variant="top" src={TabImage} />
-                        <Card.Text>
-                            Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
-                        </Card.Text>
-                        <Card.Link href="#">
-                            <i className="fab fa-facebook-f"></i>
-                            <i className="fab fa-twitter"></i>
-                        </Card.Link>
+                    <Card.Body className="tab">
+                        <Card.Img variant="top" src={TabImage} className="tab__img" />
+                        <div className="tab__content">
+                            <Card.Text className="tab__text">
+                                Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
+                            </Card.Text>
+                            <Card.Link href="#" className="tab__icons">
+                                <p>SHARE</p>
+                                <i className="fab fa-facebook-f"></i>
+                                <i className="fab fa-twitter"></i>
+                            </Card.Link>
+                        </div>
                     </Card.Body>
                 </Card>
             </Tab>
             <Tab eventKey="third" title="Third">
                 <Card>
-                    <Card.Body>
-                        <Card.Img variant="top" src={TabImage} />
-                        <Card.Text>
-                            Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
-                        </Card.Text>
-                        <Card.Link href="#">
-                            <i className="fab fa-facebook-f"></i>
-                            <i className="fab fa-twitter"></i>
-                        </Card.Link>
+                    <Card.Body className="tab">
+                        <Card.Img variant="top" src={TabImage} className="tab__img"/>
+                        <div className="tab__content">
+                            <Card.Text className="tab__text">
+                                Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
+                            </Card.Text>
+                            <Card.Link href="#" className="tab__icons">
+                                
+                                    <p>SHARE</p>
+                                    <i className="fab fa-facebook-f"></i>
+                                    <i className="fab fa-twitter"></i>
+                                
+                            </Card.Link>
+                        </div>
                     </Card.Body>
                 </Card>
             </Tab>
