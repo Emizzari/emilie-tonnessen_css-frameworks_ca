cb76dc91518b88b63d1456a61c9c28bdd3259036
Emizzari <emilie@tonnessen.com>
2020-09-10 13:38:35 +0200

    Styling Contact Form

--- src/components/contact/content/ContactForm.js
+++ src/components/contact/content/ContactForm.js
@@ -4,17 +4,23 @@ import {Form, Button, FormControl, InputGroup} from 'react-bootstrap';
 function ContactForm() {
     return (
         <>
-            <Form>
-                <Form.Group>
-                    <Form.Label>Name</Form.Label>
+            <Form className="form">
+                <Form.Group className="form__name">
+                    <Form.Label className="form__label">Name</Form.Label>
                     <Form.Control placeholder="First name" />
                 </Form.Group>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                    <Form.Label>Email</Form.Label>
+
+
+
+                <Form.Group className="form__mail" controlId="exampleForm.ControlInput1">
+                    <Form.Label className="form__label">Email</Form.Label>
                     <Form.Control type="email" placeholder="name@example.com" />
                 </Form.Group>
-                <Form.Group>
-                    <Form.Label>Website</Form.Label>
+
+
+
+                <Form.Group className="form__website">
+                    <Form.Label className="form__label">Website</Form.Label>
                     <InputGroup className="mb-2">
                         <InputGroup.Prepend>
                             <InputGroup.Text>https://</InputGroup.Text>
@@ -22,14 +28,22 @@ function ContactForm() {
                         <FormControl id="inlineFormInputGroup" placeholder="www.example.com" />
                     </InputGroup>
                 </Form.Group>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Message</Form.Label>
+
+
+
+                <Form.Group className="form__msg" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label className="form__label">Message</Form.Label>
                     <Form.Control as="textarea" rows="3" />
                 </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Allow us to sell your personal details to whomever we want" />
+
+
+                <Form.Group className="form__checkbox" controlId="formBasicCheckbox">
+                    <Form.Check label="Allow us to sell your personal details to whomever we want" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
+
+
+
+                <Button className="form__btn" variant="primary" type="submit">
                     Submit
                 </Button>
             </Form>
