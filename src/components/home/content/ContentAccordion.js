8bbc2a60da03efd947ceecb7126ef76c15f17c6a
Emizzari <emilie@tonnessen.com>
2020-09-10 12:12:00 +0200

    Styling accordion

--- src/components/home/content/ContentAccordion.js
+++ src/components/home/content/ContentAccordion.js
@@ -7,21 +7,25 @@ import TabImage from '../images/tab-image.png';
 function ContentAccordion() {
     return (
         <>
-            <Accordion defaultActiveKey="0">
-                <Card>
-                    <Card.Header>
+            <Accordion defaultActiveKey="0" className="container--accordion">
+
+
+
+                <Card className="accordion">
+                    <Card.Header className="accordion__header">
                         <Accordion.Toggle as={Button} variant="link" eventKey="0">
                             First
                         </Accordion.Toggle>
                     </Card.Header>
                     <Accordion.Collapse eventKey="0">
                         <>
-                        <Card.Body>
-                            <Card.Img variant="top" src={TabImage} />
-                            <Card.Text>
+                        <Card.Body className="accordion__body">
+                            <Card.Text className="accordion__text">
                                 Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. 
                             </Card.Text>
-                            <Card.Link href="#">
+                            <Card.Img className="accordion__img" variant="top" src={TabImage} />
+                            <Card.Link href="#" className="accordion__icons">
+                                <p>SHARE</p>
                                 <i className="fab fa-facebook-f"></i>
                                 <i className="fab fa-twitter"></i>
                             </Card.Link>
@@ -29,20 +33,24 @@ function ContentAccordion() {
                         </>
                     </Accordion.Collapse>
                 </Card>
-                <Card>
-                    <Card.Header>
+
+
+
+                <Card className="accordion">
+                    <Card.Header className="accordion__header">
                         <Accordion.Toggle as={Button} variant="link" eventKey="1">
                             Second
                         </Accordion.Toggle>
                     </Card.Header>
                     <Accordion.Collapse eventKey="1">
                         <>
-                        <Card.Body>
-                            <Card.Img variant="top" src={TabImage} />
-                            <Card.Text>
-                                Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
+                        <Card.Body className="accordion__body">
+                            <Card.Text className="accordion__text">
+                                Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. 
                             </Card.Text>
-                            <Card.Link href="#">
+                            <Card.Img className="accordion__img" variant="top" src={TabImage} />
+                            <Card.Link href="#" className="accordion__icons">
+                                <p>SHARE</p>
                                 <i className="fab fa-facebook-f"></i>
                                 <i className="fab fa-twitter"></i>
                             </Card.Link>
@@ -50,20 +58,26 @@ function ContentAccordion() {
                         </>
                     </Accordion.Collapse>
                 </Card>
-                <Card>
-                    <Card.Header>
+
+
+
+
+
+                <Card className="accordion">
+                    <Card.Header className="accordion__header">
                         <Accordion.Toggle as={Button} variant="link" eventKey="2">
                             Third
                         </Accordion.Toggle>
                     </Card.Header>
                     <Accordion.Collapse eventKey="2">
                         <>
-                        <Card.Body>
-                            <Card.Img variant="top" src={TabImage} />
-                            <Card.Text>
+                        <Card.Body className="accordion__body">
+                            <Card.Text className="accordion__text">
                                 Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. 
                             </Card.Text>
-                            <Card.Link href="#">
+                            <Card.Img className="accordion__img" variant="top" src={TabImage} />
+                            <Card.Link href="#" className="accordion__icons">
+                                <p>SHARE</p>
                                 <i className="fab fa-facebook-f"></i>
                                 <i className="fab fa-twitter"></i>
                             </Card.Link>
@@ -71,6 +85,10 @@ function ContentAccordion() {
                         </>
                     </Accordion.Collapse>
                 </Card>
+
+
+
+
             </Accordion>
         </>
     )
